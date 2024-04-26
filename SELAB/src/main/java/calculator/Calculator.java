345443688e2f1e1e571b997740953dfb65650920
Sadeq Mahmoud Abadi <sa.mahmoudabadi@gmail.com>
2024-04-26 14:21:30 +0330

    Add calculator function input two number and an operation, then get the result

--- SELAB/src/main/java/calculator/Calculator.java
+++ SELAB/src/main/java/calculator/Calculator.java
@@ -4,4 +4,15 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int calculator(int a, int b, char operation) {
+        if (operation == '*')
+            return a * b;
+        else if (operation == '/')
+            return a / b;
+        else if (operation == '^')
+            return (int)Math.pow(a, b);
+        else
+            throw new java.lang.Error("Invalid operation!");
+    }
 }
