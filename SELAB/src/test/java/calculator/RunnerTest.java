f84da979cee4c81ac185f81468e6a24892b07eb1
Sadeq Mahmoud Abadi <sa.mahmoudabadi@gmail.com>
2024-04-26 15:01:51 +0330

    Update dependencies to run maven test successfully

--- SELAB/src/test/java/calculator/RunnerTest.java
+++ SELAB/src/test/java/calculator/RunnerTest.java
@@ -1,7 +1,7 @@
 package calculator;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @CucumberOptions(features = "src/test/resources/features")
